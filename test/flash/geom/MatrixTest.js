eb069bf5725e79896ee2095b4b59d0b0055536b4
ienaga <ienaga@tvon.jp>
2017-09-07 02:49:27 +0900

    edit test case

--- test/flash/geom/MatrixTest.js
+++ test/flash/geom/MatrixTest.js
@@ -2,10 +2,28 @@ describe("Matrix.js test", function() {
 
     var matrix = new Matrix();
 
-    console.log(matrix);
-
-    it("case a", function() {
+    it("Matrix propertie test [a]", function() {
         expect(matrix.a).toBe(1);
+    });
+
+    it("Matrix propertie test [b]", function() {
+        expect(matrix.b).toBe(0);
+    });
+
+    it("Matrix propertie test [c]", function() {
+        expect(matrix.c).toBe(0);
+    });
+
+    it("Matrix propertie test [d]", function() {
+        expect(matrix.d).toBe(1);
+    });
+
+    it("Matrix propertie test [tx]", function() {
+        expect(matrix.tx).toBe(0);
+    });
+
+    it("Matrix propertie test [ty]", function() {
+        expect(matrix.ty).toBe(0);
     });
 
 
